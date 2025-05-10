0385b3cdbbe3d0f1f9f56e7b90630d1738258de5
heitzlki <kirill.heitzler@gmail.com>
2025-05-10 13:04:02 +0200

    test llm

---
+++ server/llm.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+
+# from dotenv import load_dotenv
+
+# import os
+
+# Load environment variables from .env file
+# load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="",
+)
+
+completion = client.chat.completions.create(
+    extra_headers={
+        "HTTP-Referer": "<YOUR_SITE_URL>",  # Optional. Site URL for rankings on openrouter.ai.
+        "X-Title": "<YOUR_SITE_NAME>",  # Optional. Site title for rankings on openrouter.ai.
+    },
+    model="openai/gpt-4o",
+    messages=[{"role": "user", "content": "What is the meaning of life?"}],
+)
+
+print(completion.choices[0].message.content)
